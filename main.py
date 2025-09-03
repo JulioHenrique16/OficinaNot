1a095c9374b7b7ccb7057ff911fbf15b0831e306
JulioHenrique16 <cc24473@g.unicamp.br>
2025-09-03 20:24:23 -0300

    atualizei o main.py
    fiz o q me mandaram

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
-idade = int(input())
-if idade < 18:
-    print("menor de idade")
-else:
-    print("maior de idade")
+num = int(input"idade:"))
+
+def idade(idade):
+    idade = int(input())
+    if idade < 18:
+        print("menor de idade")
+    else:
+        print("maior de idade")
+
+print (idade(num))
